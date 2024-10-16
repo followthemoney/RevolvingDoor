9f40f803c38a5165af940e17e3f0d99111ae135c
naelopode <nael.dillenbourg@epfl.ch>
2024-10-16 11:13:31 +0200

    update rss to check all transparency links

--- rss.py
+++ rss.py
@@ -10,6 +10,12 @@ import requests
 from datetime import datetime
 import random
 from urllib.parse import urlparse
+import LLM
+import requests
+from bs4 import BeautifulSoup
+import uuid
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 class NewsChecker:
     def __init__(self, config_path):
@@ -19,17 +25,83 @@ class NewsChecker:
         self.client = MongoClient(CONFIG['db_url'])
         self.db = self.client[CONFIG['db_name']]
         self.col_feed = self.db['rss_feeds']
-        self.col_entries = self.db['rss_entries_2']
+        self.col_entries = self.db['rss_entries_3']
         self.col_transparency = self.db['transparency']
         self.wait_between_fetch = CONFIG['RSS_wait_betwen_fetch']
         self.logs = LogsWriter(CONFIG)
         self.logs.info("RSS - Starting Google Alerts RSS Fetcher...")
         self.url_proxies = []
-        for prox in self.__get_proxies():
-            self.url_proxies.append({'http':f"socks5://{prox['username']}:{prox['password']}@{prox['ip']}:{prox['port']}",
-                    'https':f"socks5://{prox['username']}:{prox['password']}@{prox['ip']}:{prox['port']}"})
+        prox = self.__get_proxies()
+        self.llm_requests = []
+        self.url_proxies = {'http':f"http://{prox['username']}:{prox['password']}@{prox['ip']}:{prox['port']}",
+                'https':f"http://{prox['username']}:{prox['password']}@{prox['ip']}:{prox['port']}"}
+        self.LLM = LLM.LLMBatchProcessor(config_path)
+        self.fetch_llm_results()
         self.__check()
+        self.LLM.upload_batch()
         self.logs.info("RSS - Finished with Google Alerts RSS Fetcher.")
+
+    def __get_page_content(self, url):
+        self.logs.debug(f"RSS - Fetching URL {url}")
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+        PROXY_DATA = self.url_proxies
+        try:
+            response = requests.get(url, headers=headers, proxies=PROXY_DATA, timeout=(5, 10), verify=True)
+            response.raise_for_status()  # Handle HTTP errors
+            if 'text/html' not in response.headers.get('Content-Type', ''):
+                self.logs.error(f"RSS - Unexpected content type for URL {url}")
+                return ""
+            if len(response.content) > 10**6:  # Limit the maximum response size to 1MB
+                self.logs.error(f"RSS - Response too large for URL {url}")
+                return ""
+        except requests.exceptions.Timeout:
+            self.logs.error(f"RSS - Timeout error fetching URL {url}")
+            return ""
+        except requests.exceptions.TooManyRedirects:
+            self.logs.error(f"RSS - Too many redirects for URL {url}")
+            return ""
+        except requests.exceptions.RequestException as e:
+            self.logs.error(f"RSS - Error fetching URL {url}: {e}")
+            return ""
+        except Exception as e:
+            self.logs.error(f"RSS - Unexpected error fetching URL {url}: {e}")
+            return ""
+        
+        soup = BeautifulSoup(response.content, 'html.parser')
+        page_title = soup.title.string if soup.title else 'No Title'
+        page_contents = soup.get_text()
+        page_contents = ' '.join(page_contents.split())
+        self.logs.debug(f"RSS - Done fetching URL {url}")
+        if 'youtube.com' in url or 'youtu.be' in url:
+            video_id = url.split('%3Fv%3D')[-1].split('v%3D')[-1].split('&')[0]
+            subtitles = ""
+            try:
+                transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies = PROXY_DATA)
+                subtitles = "\n".join([entry['text'] for entry in transcript])
+            except Exception as e:
+                self.logs.error(f"RSS - Error fetching subtitles for YouTube URL {url}: {e}")
+            page_contents = page_contents + "\n\n" + subtitles
+            self.logs.debug(f"RSS - Done fetching YouTube URL {url}")
+            return page_title + "\n\n" + page_contents
+
+        return page_title + "\n\n" + page_contents
+ 
+
+    def fetch_llm_results(self):
+        results = self.LLM.return_ready_batches()
+        for sub_result in results:
+            for res in sub_result:
+                uuid = res['custom_id'].replace('request-', '')
+                if res['score'] == -1:
+                    self.col_entries.update_one(
+                        {'uuid': uuid},
+                        {'$set': {'llm_score': -1, 'llm_run': True, 'llm_state': 'error'}}
+                    )
+                if res['score'] != -1:
+                    self.col_entries.update_one(
+                        {'uuid': uuid},
+                        {'$set': {'llm_score': res['score'], 'llm_run': True, 'llm_state': 'completed'}}
+                    )
 
     def get_entries(self):
         return list(self.col_entries.find())
@@ -43,7 +115,7 @@ class NewsChecker:
     def __check_website(self, url):
         url = url.replace('https://www.google.com/url?rct=j&sa=t&url=', '')
         domain = urlparse(url).netloc.replace('www.','')
-        ignore_list = ['twitter.com', 'x.com', 'google.com', 'youtube.com', 'facebook.com', 'instagram.com', 'dailymotion.com', 'bbc.co.uk']
+        ignore_list = ['twitter.com', 'x.com', 'google.com', 'youtube.com', 'facebook.com', 'instagram.com', 'dailymotion.com', 'bbc.co.uk', 'tiktok.com']
         finding = self.col_transparency.find_one({'webSiteURL': {'$regex': f".*{domain}.*"}})
         if domain in ignore_list:
             return False
@@ -52,22 +124,28 @@ class NewsChecker:
         return False
 
     def __check(self):
+        self.logs.debug(f"RSS - Fetching News labeled entries")
         for entry in self.col_feed.find():
-            for entry in self.col_feed.find():
-                if 'constituencies_country' not in entry:
-                    entry['constituencies_country'] == '' 
-                if 'constituencies_party' not in entry:
-                    entry['constituencies_party'] == '' 
-                if 'groups_organization' not in entry:
-                    entry['groups_organization'] == '' 
-            PROXY_DATA = random.choice(self.url_proxies)
+            if 'constituencies_country' not in entry.keys():
+                entry['constituencies_country'] == '' 
+            if 'constituencies_party' not in entry.keys():
+                entry['constituencies_party'] == '' 
+            if 'groups_organization' not in entry.keys():
+                entry['groups_organization'] == '' 
+            #PROXY_DATA = random.choice(self.url_proxies)
+            PROXY_DATA = self.url_proxies
             proxy_handler = ProxyHandler(PROXY_DATA) #ADD PROXY
-            ##NEWS
-            feed = feedparser.parse(entry['news_rss'], handlers=[proxy_handler])
+            ##NEWSf
+            try:
+                feed = feedparser.parse(entry['news_rss'], handlers=[proxy_handler])
+            except:
+                continue
             for rss_element in feed.entries:
                 if not self.__already_exist(entry['userID'], hashlib.md5(rss_element.link.encode('utf-8')).hexdigest()):
                     self.logs.debug(f"RSS - Adding news entry for {entry['full_name']}")
+                    new_uuid = str(uuid.uuid4())
                     new_entry = {
+                        'uuid' : new_uuid,
                         'userID' : entry['userID'],
                         'full_name' : entry['full_name'],
                         'title' : bleach.clean(rss_element.title),
@@ -79,16 +157,36 @@ class NewsChecker:
                         'constituencies_party' : entry['constituencies_party'],
                         'groups_organization' : entry['groups_organization'],
                         'is_news' : True,
-                        'in_transparency' : False #Set news item as false as some news are in transparency register
+                        'in_transparency' : self.__check_website(rss_element.link), #Set news item as false as some news are in transparency register,
+                        'llm_run' : False,
+                        'llm_state' : 'pending',
+                        'llm_score' : 0,
+                        'read' : False,
+                        'pinned' : False
                     }
+                    #print(f"TRYING TO REACH {rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0]}")
                     self.col_entries.insert_one(new_entry)
-            sleep(self.wait_between_fetch)
+                    try:
+                        page_content = self.__get_page_content(rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0])
+                        page_content = page_content + "\n\n" + new_entry['summary']
+                        print(page_content)
+                        self.llm_requests.append({'name':entry['full_name'], 'page_text': page_content, 'uuid': new_uuid})
+                    except Exception as e:
+                        self.logs.error(f"RSS - Error Fetching URL {rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0]}")
+            if feed.entries:
+                self.logs.debug(f"RSS - Sleeping for {self.wait_between_fetch} seconds...")
+                sleep(self.wait_between_fetch)
             ##ALL
-            feed = feedparser.parse(entry['rss'], handlers=[proxy_handler])
+            try:
+                feed = feedparser.parse(entry['rss'], handlers=[proxy_handler])
+            except:
+                continue
             for rss_element in feed.entries:
                 if not self.__already_exist(entry['userID'], hashlib.md5(rss_element.link.encode('utf-8')).hexdigest()):
                     self.logs.debug(f"RSS - Adding News entry for {entry['full_name']}")
+                    new_uuid = str(uuid.uuid4())
                     new_entry = {
+                        'uuid' : new_uuid,
                         'userID' : entry['userID'],
                         'full_name' : entry['full_name'],
                         'title' : bleach.clean(rss_element.title),
@@ -101,29 +199,34 @@ class NewsChecker:
                         'groups_organization' : entry['groups_organization'],
                         'is_news' : False,
                         'in_transparency' : self.__check_website(rss_element.link),
+                        'llm_run' : False,
+                        'llm_state' : 'pending',
+                        'llm_score' : 0,
+                        'read' : False,
+                        'pinned' : False
                     }
+                    #print(f"TRYING TO REACH {rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0]}")
                     self.col_entries.insert_one(new_entry)
-            sleep(self.wait_between_fetch)
+                    try:
+                        page_content = self.__get_page_content(rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0])
+                        page_content = page_content + "\n\n" + new_entry['summary']
+                        print(page_content)
+                        self.llm_requests.append({'name':entry['full_name'], 'page_text': page_content, 'uuid': new_uuid})
+                    except Exception as e:
+                        self.logs.error(f"RSS - Error Fetching URL {rss_element.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=')[0]}")
+            if feed.entries:
+                self.logs.debug(f"RSS - Sleeping for {self.wait_between_fetch} seconds...")
+                sleep(self.wait_between_fetch)
 
+        # Step 1: Create the JSONL batch file
+        if self.llm_requests != []:
+            self.LLM.create_batch_jsonl(self.llm_requests)
+            
     def __get_proxies(self):
-        try:
-            response = requests.get(
-                "https://proxy.webshare.io/api/v2/proxy/list/?mode=direct&page=1&page_size=100",
-                headers={"Authorization": f"Token {self.CONFIG['webshare_token']}"}
-            )
-            response.raise_for_status()  # Raise an HTTPError for bad responses
-        except requests.RequestException as e:
-            print(f"Failed to get proxy: {e}")
-            return
-        PROXIES = []
-        for res in response.json()['results']:
-            PROXIES.append({
-                    'ip' : res['proxy_address'],
-                    'port' : res['port'],
-                    'username' : res['username'],
-                    'password' : res['password']
-                })
-        return PROXIES
-
-
+            return {
+                    'ip' : 'p.webshare.io',
+                    'port' : '80',
+                    'username' : self.CONFIG['webshare_usr'],
+                    'password' : self.CONFIG['webshare_pwd']
+                }
 NewsChecker('./config.json')
